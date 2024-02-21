0e258daabb52c9dbf15e7e262cffb343c24796b9
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-21 14:39:07 +0300

    basic file concatenation

--- practice_16/practice_16_02.cpp
+++ practice_16/practice_16_02.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 int main(int argc, char* argv[]) {
-    return 0;
+    if (argc <  2) {
+        std::cerr << "Usage: " << argv[0] << " [FILE]...\n";
+        return  1;
+    }
+
+    for (int i =  1; i < argc; ++i) {
+        std::ifstream file(argv[i]);
+        if (!file) {
+            std::cerr << "Error: Cannot open file " << argv[i] << '\n';
+            return  1;
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            std::cout << line;
+            if (!file.eof()) {
+                std::cout << '\n';
+            }
+        }
+
+        file.close();
+    }
+
+    return  0;
 }
