38d51c81afec336879af76d8430d7c55dc5fb5a0
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-21 16:39:15 +0300

    add format

--- practice_16/practice_16_02.cpp
+++ practice_16/practice_16_02.cpp
@@ -2,41 +2,41 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cstring>  
+#include <cstring>
+#include <iomanip> 
 
 int main(int argc, char* argv[]) {
     bool numberLines = false;
-    int fileIndex =  1;
+    int fileIndex =   1;
 
-    // Check for the -n option
-    if (argc >  1 && std::strcmp(argv[1], "-n") ==  0) {
+    if (argc >   1 && std::strcmp(argv[1], "-n") ==   0) {
         numberLines = true;
         fileIndex++;
     }
 
-    if (argc < fileIndex +  1) {
+    if (argc < fileIndex +   1) {
         std::cerr << "Usage: " << argv[0] << " [-n] [FILE]...\n";
-        return  1;
+        return   1;
     }
 
     for (int i = fileIndex; i < argc; ++i) {
         std::istream* input = nullptr;
-        if (std::strcmp(argv[i], "-") ==  0) {
+        if (std::strcmp(argv[i], "-") ==   0) {
             input = &std::cin;
         } else {
             input = new std::ifstream(argv[i]);
             if (!*input) {
                 std::cerr << "Error: Cannot open file " << argv[i] << '\n';
                 delete input;
-                return  1;
+                return   1;
             }
         }
 
         std::string line;
-        int lineNumber =  1;
+        int lineNumber =   1;
         while (std::getline(*input, line)) {
             if (numberLines) {
-                std::cout << lineNumber++ << ": ";
+                std::cout << std::setw(4) << std::setfill(' ') << lineNumber++ << ": ";
             }
             std::cout << line;
             if (!input->eof()) {
@@ -49,5 +49,5 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    return  0;
+    return   0;
 }
