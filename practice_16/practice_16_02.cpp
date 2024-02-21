fdb825f46047d3526a408645d9b98d3f9e449b33
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-21 12:38:27 +0300

    back to the rus

--- practice_16/practice_16_02.cpp
+++ practice_16/practice_16_02.cpp
@@ -1,31 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-void print_file(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return;
-    }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
-
-    file.close();
-}
-
 int main(int argc, char* argv[]) {
-    if (argc <  2) {
-        std::cerr << "Usage: " << argv[0] << " [file1] ..." << std::endl;
-        return  1;
-    }
-
-    for (int i =  1; i < argc; ++i) {
-        print_file(argv[i]);
-    }
-
     return  0;
 }
