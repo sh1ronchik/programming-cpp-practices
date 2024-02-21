99bbfdba8117b2364d1267ca5a196f4f7805eee4
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-21 15:02:07 +0300

    realise standart input

--- practice_16/practice_16_02.cpp
+++ practice_16/practice_16_02.cpp
@@ -2,30 +2,39 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstring> 
 
 int main(int argc, char* argv[]) {
-    if (argc <  2) {
+    if (argc <   2) {
         std::cerr << "Usage: " << argv[0] << " [FILE]...\n";
-        return  1;
+        return   1;
     }
 
-    for (int i =  1; i < argc; ++i) {
-        std::ifstream file(argv[i]);
-        if (!file) {
-            std::cerr << "Error: Cannot open file " << argv[i] << '\n';
-            return  1;
+    for (int i =   1; i < argc; ++i) {
+        std::istream* input = nullptr;
+        if (std::strcmp(argv[i], "-") ==   0) {
+            input = &std::cin;
+        } else {
+            input = new std::ifstream(argv[i]);
+            if (!*input) {
+                std::cerr << "Error: Cannot open file " << argv[i] << '\n';
+                delete input;
+                return   1;
+            }
         }
 
         std::string line;
-        while (std::getline(file, line)) {
+        while (std::getline(*input, line)) {
             std::cout << line;
-            if (!file.eof()) {
+            if (!input->eof()) {
                 std::cout << '\n';
             }
         }
 
-        file.close();
+        if (input != &std::cin) {
+            delete input;
+        }
     }
 
-    return  0;
+    return 0;
 }
